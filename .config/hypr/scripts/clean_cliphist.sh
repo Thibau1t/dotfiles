1ce21c455d97480113e400901f3a3f3ba7d4a87b
Thibau1t <thibau1t.villepreux@gmail.com>
2024-08-18 16:01:50 +0200

    add wallpapers repertory

---
+++ .config/hypr/scripts/clean_cliphist.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sleep 1
+cliphist wipe
+sleep 1
+rm -rf $HOME/.cache/cliphist
--- .config/hypr/scripts/restart_pipewire.sh
+++ .config/hypr/scripts/restart_pipewire.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 sleep 1
 systemctl --user restart pipewire pipewire-pulse wireplumber
-
